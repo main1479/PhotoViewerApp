e7c9847112c5e0d884a28c3f03956b217146b24e
main1479 <m.main2402@gmail.com>
2021-05-02 14:17:33 +0600

    initial project done

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,30 @@
 const navTrigger = document.querySelectorAll('.menu__trigger');
-const menu = document.querySelector('.nav__right')
+const menu = document.querySelector('.nav__right');
 
-navTrigger.forEach(btn => {
-   btn.addEventListener('click', ()=> {
-      menu.classList.toggle('active')
-   })
-})
+navTrigger.forEach((btn) => {
+	btn.addEventListener('click', () => {
+		menu.classList.toggle('active');
+	});
+});
 
-
-// video 
+// video
 
 const videos = document.querySelectorAll('.video');
 
-videos.forEach(video => {
-   video.addEventListener('click', (e) => {
-      video.play();
-      setTimeout(()=> video.controls = 'true', 1000)
-   })
-})
+videos.forEach((video) => {
+	video.addEventListener('click', (e) => {
+		video.play();
+		setTimeout(() => (video.controls = 'true'), 1000);
+	});
+});
+
+// slideshow
+
+const slideshow = document.querySelectorAll('.btn__slideshow');
+
+slideshow.forEach((btn) => {
+	btn.addEventListener('click', function () {
+		document.body.classList.toggle('slideshow__full');
+      swiper2.update()
+	});
+});
